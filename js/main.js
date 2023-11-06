a1cdf12f18f7fd2ebaf8c3b93023223965273e88
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-06 17:14:25 +0100

    last commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
 "use strict";
 
-//* Inserimento dei dati da parte dell' utente
-//* Trasformazione dei dati inseriti da stringhe a numeri con parseInt()
 const km = parseInt(prompt("Quanti Km devi percorrere?"));
 console.log("Km: " + km);
 
 const age = parseInt(prompt("Inesrisci qui la tua età"));
 console.log("Età: " + age);
 
-const price = 0.21 * km;
+const price = parseFloat(0.21 * km).toFixed(2);
 console.log("Prezzo base del biglietto: " + price);
 
 let discount = 0;
 
-const discountPrice = price - (price * discount) / 100;
+const discountPrice = parseFloat(price - (price * discount) / 100).toFixed(2);
 
 if (age < 18) {
   discount = 20;
-  const discountPrice = price - (price * discount) / 100;
+  const discountPrice = parseFloat(price - (price * discount) / 100).toFixed(2);
   console.log(
     "Il prezzo del biglietto è: " +
       discountPrice +
@@ -25,7 +23,7 @@ if (age < 18) {
   );
 } else if (age > 65) {
   discount = 40;
-  const discountPrice = price - (price * discount) / 100;
+  const discountPrice = parseFloat(price - (price * discount) / 100).toFixed(2);
   console.log(
     "Il prezzo del biglietto è: " +
       discountPrice +
