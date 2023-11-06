64e6d186bb9dddd7f3dd9434694e6949a1526405
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-06 16:46:05 +0100

    age discount

--- js/main.js
+++ js/main.js
@@ -8,5 +8,19 @@ console.log("Km: " + km);
 const age = parseInt(prompt("Inesrisci qui la tua età"));
 console.log("Età: " + age);
 
-const prova = km + age;
-console.log(prova);
+const price = 0.21 * km;
+console.log("Prezzo base del biglietto: " + price);
+
+let discount = 0;
+
+const discountPrice = price - (price * discount) / 100;
+
+if (age < 18) {
+  discount = 20;
+  const discountPrice = price - (price * discount) / 100;
+  console.log("Il prezzo del biglietto è: " + discountPrice);
+} else if (age > 65) {
+  discount = 40;
+  const discountPrice = price - (price * discount) / 100;
+  console.log("Il prezzo del biglietto è: " + discountPrice);
+}
