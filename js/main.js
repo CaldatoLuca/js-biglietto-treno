2d5300bf1e9ac7a0e88da4f5931220c9adab9458
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-06 16:26:07 +0100

    parseInt to Km and Age

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 "use strict";
 
 //* Inserimento dei dati da parte dell' utente
-const km = prompt("Quanti Km devi percorrere?");
+//* Trasformazione dei dati inseriti da stringhe a numeri con parseInt()
+const km = parseInt(prompt("Quanti Km devi percorrere?"));
 console.log("Km: " + km);
 
-const age = prompt("Inesrisci qui la tua età");
+const age = parseInt(prompt("Inesrisci qui la tua età"));
 console.log("Età: " + age);
+
+const prova = km + age;
+console.log(prova);
