35c8b7107ab5858b9bca94f08b3eea7ef3763549
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-06 16:19:39 +0100

    feat: km and age prompt

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("gdgdg");
+
+//* Inserimento dei dati da parte dell' utente
+const km = prompt("Quanti Km devi percorrere?");
+console.log("Km: " + km);
+
+const age = prompt("Inesrisci qui la tua età");
+console.log("Età: " + age);
