c436b10d420eb314f0163db7019114d177fdde4e
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-07 11:25:26 +0100

    correzione

--- js/main.js
+++ js/main.js
@@ -36,3 +36,34 @@ if (age < 18) {
       " - nessuna scontistica applicata"
   );
 }
+
+// ("use strict");
+// // ! number, ci da un numero (anche decimale) invece di una stringa ma da NaN se utente usa lettere nel prompt
+// const kmRichiesti = Number(prompt("Quanti Km devi percorrere?"));
+// const etaPasseggiero = Number(prompt("Inesrisci qui la tua età"));
+// const prezzoKm = 0.21;
+// const percentualeScontoMinorenni = 20;
+// const percentualeScontoOver65 = 40;
+// // ! mettere tra graffe la variablile migliora il console.log
+// console.log({ kmRichiesti });
+// console.log({ etaPasseggiero });
+
+// // ! not isNas (quando è un numero faccio if, altrimenti else)
+// // ! uso negazione cosi programma principale sta su if
+// // ! isNan = è un Nan?
+// // ! !isNan = non è un Nan? quindi è un numero
+// if (!isNaN(kmRichiesti) && !isNaN(etaPasseggiero)) {
+//   let prezzoBiglietto = kmRichiesti * prezzoKm;
+//   let scontoBiglietto = 0;
+
+//   if (etaPasseggiero < 18) {
+//     scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoMinorenni;
+//   } else if (etaPasseggiero >= 65) {
+//     scontoBiglietto = (prezzoBiglietto / 100) * percentualeScontoOver65;
+//   }
+
+//   prezzoBiglietto -= scontoBiglietto;
+//   console.log("Il prezzo del biglietto è: " + prezzoBiglietto.toFixed(2));
+// } else {
+//   console.log("Errore");
+// }
